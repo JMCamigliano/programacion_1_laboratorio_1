9f43163b15b4f537828e1a2bf19b93ffaf923963
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-08-25 10:42:41 -0300

    Clase 1
    Prueba

---
+++ Clase_01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int edad;
+    printf("Ingrese su edad: ");
+    scanf("%d", &edad);
+
+    printf("La edad es: %d",edad);
+
+    return 0;
+}
